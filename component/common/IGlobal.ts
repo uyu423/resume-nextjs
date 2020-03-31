d6d4b10abf4d940fcdb51742e4223a49e9a95e59
Yowu <me@yowu.dev>
2020-04-01 03:19:43 +0900

    누락된 IGlobal TypeDoc 을 추가했다.

--- component/common/IGlobal.ts
+++ component/common/IGlobal.ts
@@ -2,9 +2,24 @@ import { NextSeoProps } from 'next-seo';
 
 export declare namespace IGlobal {
   export interface Payload {
+    /**
+     * ### Resume 제목
+     *
+     * @description `<head>` 태그 내 `<title>` 을 정의한다.
+     */
     headTitle: string;
+
+    /**
+     * ### favicon 이미지
+     */
     favicon: string;
-    /** @see https://github.com/garmeeh/next-seo */
+
+    /**
+     * ### SEO Properties
+     *
+     * @description OpenGraph 메타 태그를 포함한 SEO 를 위한 요소를 정의한다. `next-seo` 를 사용했으므로 해당 패키지를 참고한다.
+     * @see https://github.com/garmeeh/next-seo
+     */
     seo: NextSeoProps;
   }
 }
