c1881fc8ec38f2ab41f1092036b034d64c6b06df
Yu Yongwoo <me@yowu.dev>
2020-04-13 14:33:12 +0900

    CNAME 수정

--- payload/openSource.ts
+++ payload/openSource.ts
@@ -1,64 +1,5 @@
 import { IOpenSource } from '../component/openSource/IOpenSource';
 
-const openSource: IOpenSource.Payload = {
-  disable: false,
-  list: [
-    {
-      title: 'node',
-      descriptions: [
-        { content: 'Node.js JavaScript runtime' },
-        { content: 'Contributor' },
-        {
-          content: 'https://github.com/nodejs/node',
-          href: 'https://github.com/nodejs/node',
-        },
-        {
-          content: 'Github Stars: ',
-          postImage: 'https://img.shields.io/github/stars/nodejs/node.svg?style=popout',
-        },
-      ],
-    },
-    {
-      title: 'NestJS',
-      descriptions: [
-        {
-          content: 'A progressive Node.js framework',
-        },
-        {
-          content:
-            'for building efficient, scalable, and enterprise-grade server-side applications',
-        },
-        {
-          content: 'Contributor',
-        },
-        {
-          content: 'https://github.com/nestjs/nest',
-          href: 'https://github.com/nestjs/nest',
-        },
-        {
-          content: 'Monthly Download: ',
-          postImage: 'https://img.shields.io/npm/dm/@nestjs/core.svg',
-        },
-        {
-          content: 'Github Stars: ',
-          postImage: 'https://img.shields.io/github/stars/nestjs/nest.svg?style=popout',
-        },
-      ],
-    },
-    {
-      title: 'Developer Community',
-      descriptions: [
-        {
-          content: 'Presentation in 2019',
-        },
-        {
-          content: 'Hackathon participation in 2017',
-        },
-      ],
-    },
-  ],
-};
-
 const r2curl: IOpenSource.Item = {
   title: 'r2curl',
   descriptions: [
@@ -190,7 +131,7 @@ const nineXdDeveloperComunity: IOpenSource.Item = {
 
 const openSource: IOpenSource.Payload = {
   disable: true,
-  list: [resume, r2curl, nodeEscpos, kanboard, ossDeveloperComunity, nineXdDeveloperComunity],
+  list: [r2curl, nodeEscpos, kanboard, ossDeveloperComunity, nineXdDeveloperComunity],
 };
 
 export default openSource;
