e9f6649562c781f79f51e5e4233f8c80799b93a6
Kim Dae Hee <anyozcall@gmail.com>
2020-04-22 21:53:38 +0900

    ISkill.ts Comment Update
    ISKill value 1 Comment <-> Value 3 Comment Change

--- component/skill/ISkill.ts
+++ component/skill/ISkill.ts
@@ -36,9 +36,9 @@ export declare namespace ISkill {
     /**
      * ### 보유 기술 수준
      *
-     * @value 1: 파란색 뱃지로 나타난다. (primary)
+     * @value 1: 옅은 회색 뱃지로 나타난다. (light)
      * @value 2: 짙은 회색 뱃지로 나타난다. (sencondary)
-     * @value 3: 옅은 회색 뱃지로 나타난다. (light)
+     * @value 3: 파란색 뱃지로 나타난다. (primary)
      * @value undefined: 뱃지가 붙지 않는다.
      */
     level?: 1 | 2 | 3;
