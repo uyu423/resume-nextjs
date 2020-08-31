91dc36756e2357ac4124c48e44044c4664c1194f
Yu Yongwoo <me@yowu.dev>
2020-08-31 22:56:17 +0900

    Re-generate docs

--- docs/_next/static/ENA_coupOSqIiNWPXO1uP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c]}}("static\u002Fchunks\u002Fa9a7754c.962d5a43d4edc309387f.js","static\u002Fchunks\u002Fcb1608f2.ecd8534da2a32f25b740.js","static\u002Fchunks\u002F0f1ac474.a6dd1ffba7207b456c7a.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
