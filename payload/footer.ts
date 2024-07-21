d98774f48d83ebb103afe7f1c17730bcef0092ce
Yongwoo Yu (Yowu) <me@yowu.dev>
2024-07-22 03:19:17 +0900

    fix build error

--- payload/footer.ts
+++ payload/footer.ts
@@ -1,14 +1,14 @@
 import { homepage, version } from '../package.json';
 
 import { IFooter } from '../component/footer/IFooter';
-import { dependencies } from '../package-lock.json';
+import { packages } from '../package-lock.json';
 
 const footer: IFooter.Payload = {
   version,
   github: homepage,
-  nextVersion: dependencies.next.version,
-  reactVersion: dependencies.react.version,
-  bootstrapVersion: dependencies.bootstrap.version,
+  nextVersion: packages[''].dependencies.next,
+  reactVersion: packages[''].dependencies.react,
+  bootstrapVersion: packages[''].dependencies.bootstrap,
 };
 
 export default footer;
